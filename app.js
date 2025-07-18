e362b737e3df8dcc4425d66da558dcc17c6ce2a8
Chryso <chrysoBeatz@gmail.com>
2025-07-18 11:17:11 +0000

    small express.js app

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const express = require("express");
 const app = express();
 
 
-const port = process.env.PORT;
-
+function logAgent(req, res, next) {
+    console.log("🔍 logAgent");
+    next()
+}
+function authRequired(req, res, next) {
+    console.log("🔐 authRequired");
+    if(req.query.user === "admin")
+    {
+        next()
+    }
+    else {
+        res.status(400).type("html").send("<h1>Forbidden</h1>")
+    }
+}
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    console.log("Home is delivered");
+    res.status(200).type("html").send("<h1>Hello! This is a Home page</h1>")
 })
 
+app.get("/admin", logAgent, authRequired, (req, res) => {
+    res.status(200).type("html").send("<h1>Welcome, Admin</h1>")
+})
+
+
+const port = process.env.PORT;
+
 app.listen(port, () => {
-    console.log("Server is running");
+    console.log("Server is running")
 })
